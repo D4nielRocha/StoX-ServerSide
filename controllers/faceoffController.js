c21445894bb5a3b85054fcbafdf02efa19d99081
Daniel Rocha <x00170715@mytudublin.ie>
2021-04-20 17:01:52 +0100

    added authentication middleware

--- controllers/faceoffController.js
+++ controllers/faceoffController.js
@@ -48,33 +48,3 @@ router.post('/', checkJwt, async (req, res) => {
 
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-// router.get('/', (req, res) => {
-//     // res.send('STOCK COMPARISON PAGE!! COMING UP!!');
-//     res.render('faceoffService');
-// })
-
-// //route with variable path.
-// router.get('/:comparison', (req, res) => {
-//     //access variable from req.params = destructuring
-//     const { comparison } = req.params;
-//     //use variable
-//     res.send(`This is a comparison of ${comparison}`);
-// })
-
-// router.get('/:comparison/result/:resultID', (req, res) => {
-//     //access variable from req.params = destructuring
-//     const { comparison, resultID } = req.params;
-//     //use variable
-//     res.send(`This is the ${resultID} comparison of ${comparison}`);
-// })
